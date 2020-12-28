08115a95567336f7f9621a17ef305467bdd8da41
Harry <harryhoho93@gmail.com>
2020-12-28 12:29:59 -0800

    JS: Home contact btn

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 // Make navbar transparent when it is on the top
 
 const navbar = document.querySelector('#navbar');
@@ -16,16 +15,32 @@ document.addEventListener('scroll', () => {
   }
 });
 
+
+// Scroll function
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+};
+
 // Handle scrolling when tapping on the navbar menu
 
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
+
   if (link == null) {
     return;
   } 
   // console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
+});
+
